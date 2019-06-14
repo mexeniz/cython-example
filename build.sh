e8042d2dffb87ef3b58f006ac86adc0acffd6df9
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2019-06-14 23:41:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+REPO_PATH=$(pwd)
 
 # Copy source file
+echo "==> Copying source"
 rm -rf build
 mkdir build
-cp -r src/myapi build
+cp -r src/myapi src/tests build
 
 # Build module
+echo "==> Building module"
 cd build/myapi
 python setup.py build_ext --inplace
 
+# Testing 
+echo "==> Run unit testing"
+cd ${REPO_PATH}
+pytest -sv build/tests
+
 # Clean build directory and all source files except __init__.py
-rm -rf build
-find . \( -name '*.c' \) -delete
-find . \( -name '*.py' -and ! -name '__init__.py' \) -delete
+echo "==> Clean build directory"
+cd ${REPO_PATH}/build
+find . | grep -E "(build|__pycache__|\.pyc|\.pyo$|\.c$)" | xargs rm -rf
+find myapi \( -name '*.py' -and ! -name '__init__.py' \) -delete
