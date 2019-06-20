ba04948de05b1afef199aebb09e7cef68dcd80a2
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2019-06-20 23:05:43 +0700

    Add step comments

--- build.sh
+++ build.sh
@@ -2,30 +2,42 @@
 REPO_PATH=$(pwd)
 PACKAGE_NAME=myapi
 
+# Step 1:
 # Copy source file
+
 echo "==> Copying source files"
 rm -rf build
 mkdir build
 cp -r src/${PACKAGE_NAME} src/tests build
 
-# Build module
+# Step 2:
+# Build modules
+
 echo "==> Building modules"
 cd build/${PACKAGE_NAME}
 python setup.py build_ext --inplace
 
+# Step 3:
 # Clean build directory and all source files except __init__.py
+
 echo "==> Clean build directory"
 cd ${REPO_PATH}/build
 find . | grep -E "(build|__pycache__|\.pyc|\.pyo$|\.c$)" | xargs rm -rf
 find ${PACKAGE_NAME} \( -name '*.py' -and ! -name '__init__.py' \) -delete
 
-# Testing 
+# Step 4:
+# Test compiled libraries
+
 echo "==> Run unit testing"
 cd ${REPO_PATH}
 pytest -sv build/tests
 
 if [[ $? -eq "0" ]]; then
     # Previous command run successfully.
+
+    # Step 5:
+    # Archive a package if all tests pass
+
     echo "==> Passed testing, Archive a package"
     cd ${REPO_PATH}/build
     tar cvfz ${PACKAGE_NAME}.tar.gz ../requirements.txt ${PACKAGE_NAME}/*
